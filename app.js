14f9e014cfa8499bf1cce349423d338aa3751fee
asadullah696 <asadullahrajpoot19@gmail.com>
2023-09-17 19:16:39 +0500

    Full Responsive Navbar Completed

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+
+    function menuOn(){
+        let menulist = document.getElementById("menuList").style.display = "flex";
+        let oN = document.getElementById("menuOn").style.display = "none";
+        let oFf = document.getElementById("menuOff").style.display = "flex";
+    }
+    function menuOff(){
+        let menulist = document.getElementById("menuList").style.display = "none";
+        let oN = document.getElementById("menuOn").style.display = "flex";
+        let oFf = document.getElementById("menuOff").style.display = "none";
+    }
