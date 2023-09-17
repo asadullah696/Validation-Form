588e87a0b33f3015d2edb241eab021cb62a9fe26
asadullah696 <asadullahrajpoot19@gmail.com>
2023-09-18 01:33:39 +0500

    Add to javascript but failed

--- Javascript/app.js
+++ Javascript/app.js
@@ -12,3 +12,16 @@
         let oN = document.getElementById("menuOn").style.display = "flex";
         let oFf = document.getElementById("menuOff").style.display = "none";
     }
+    function checkValidation(){
+        let email = ["asadullah", "badshah", "Kings"]
+        let password = ["123", "456", "789"]
+        
+        let useremail = document.getElementById("email").value ;
+        let userpassword = document.getElementById("password").value ;
+
+        for(i=0; i<email.length; i++){
+            if(useremail != email[i]){
+                alert("BY")
+            }
+        }
+    }
